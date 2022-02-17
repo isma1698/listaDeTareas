7f302996ca18c5c3a61ab1b2d6065014a5234790
ismael <a340252566@my.uvm.edu.mx>
2022-02-17 13:20:55 -0600

    segundo commit

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ export const todoList = new TodoList();
 
 todoList.todos.forEach(crearTodoHtml);
 
-todoList.todos[0].imprimirClase();
+// todoList.todos[0].imprimirClase();
 
 //console.log('todos', todoList.todos);
